45dce36206f79eb3cf06009728b97709a7a9d4e9
andsou <andysoutcharith@gmail.com>
2022-04-02 04:00:20 -0500

    Instead of console.log to display results I added a scorecard and buttons for the user to click on to play the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,110 +1,153 @@
 function computerPlay() {
     let random = Math.random();
 
-    if (random <= 0.33) {
+    if (random <= 0.33) 
+    {
         random = "rock";
     }
-    else if (random <= 0.67) {
+    else if (random <= 0.67) 
+    {
         random = "paper";
     }
-    else {
+    else 
+    {
         random = "scissors";
     }
 
     return random;
 }
 
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection, computerSelection) 
+{
+    let h2 = document.getElementById("start");
+    const body = document.querySelector("body");
+    let result = "";
 
-    if (playerSelection == "rock" && computerSelection == "paper") {
-        return "You lose! Paper beats Rock!"
+    if (playerSelection == "rock" && computerSelection == "paper")
+    {
+        result =  "You chose Rock! Computer Chose Paper!"
     }
-    else if (playerSelection == "rock" && computerSelection == "scissors") {
-        return"You win! Rock beats scissors!";
+    else if (playerSelection == "rock" && computerSelection == "scissors") 
+    {
+        result = "You chose Rock! Computer Chose Scissors!";
     }
-    else if (playerSelection == "rock" && computerSelection == "rock"){
-        return "It's a Draw"
+    else if (playerSelection == "rock" && computerSelection == "rock")
+    {
+        result = "You chose Rock! Computer Chose Rock!"
+    }
+    else if (playerSelection == "paper" && computerSelection == "rock") 
+    {
+        result = "You chose Paper! Computer Chose Rock!"
+    }
+    else if (playerSelection == "paper" && computerSelection == "scissors") 
+    {
+        result = "You chose Paper! Computer Chose Scissors!"
+    }
+    else if (playerSelection == "paper" && computerSelection == "paper") 
+    {
+        result = "You chose Paper! Computer Chose Paper!";
+    }
+    else if (playerSelection == "scissors" && computerSelection == "paper") 
+    {
+        result = "You chose Scissors! Computer Chose Paper!";
+    } 
+    else if (playerSelection == "scissors" && computerSelection == "rock") 
+    {
+        result = "You chose Scissors! Computer Chose Rock!";
+    } 
+    else if (playerSelection == "scissors" && computerSelection == "scissors")
+     {
+        result = "You chose Scissors! Computer Chose Scissors!";
+    } 
+    else 
+    {
+        result = "Something Went Wrong";
     }
 
-    else if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You win! Paper beats Rock."
-    }
-    else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You lose! Scissors beats paper."
-    }
-    else if (playerSelection == "paper" && computerSelection == "paper") {
-        return "It's a Draw";
-    }
-
-    else if (playerSelection == "scissors" && computerSelection == "paper") {
-        return "You win! scissors beat paper";
-    } 
-    else if (playerSelection == "scissors" && computerSelection == "rock") {
-        return "You Lose! rock beats scissors";
-    } 
-    else if (playerSelection == "scissors" && computerSelection == "scissors") {
-        return "It's a Draw";
-    } 
-    else {
-        return "Something Went Wrong";
-    }
+    h2.textContent = result;
 }
 
 function game() {
     let playerScore = 0;
     let computerScore = 0;
 
+    let player = document.getElementById('player-score');
+    let computer = document.getElementById('computer-score');
 
-    for (let i = 0; i < 5; i++) {
-        const result = prompt("Choose rock, paper, or scissors").toLowerCase();
-        let computerSelection = computerPlay();
+    const buttons = document.querySelectorAll('button');
+    let result = "";
 
-        console.log(playRound(result, computerSelection));
+    buttons.forEach((button) => {
+       
+        button.addEventListener('click', () => {
 
-        if (result == "rock" && computerSelection == "paper") {
-            computerScore++;
-        }
-        else if (result == "rock" && computerSelection == "scissors") {
-            playerScore++;
-        }
-        else if (result == "rock" && computerSelection == "rock") {
-            playerScore += 0;
-            computerScore += 0;
-        } else if (result == "paper" && computerSelection == "rock") {
-            playerScore++;
-        }
-        else if (result == "paper" && computerSelection == "scissors") {
-            computerScore++;
-        }
-        else if (result == "paper" && computerSelection == "paper") {
-            playerScore += 0;
-            computerScore += 0;
-        }
-        else if (result == "scissors" && computerSelection == "paper") {
-            playerScore++;
-        }
-        else if (result == "scissors" && computerSelection == "rock") {
-            computerScore++;
-        }
-        else if (result == "scissors" && computerSelection == "scissors") {
-            playerScore += 0;
-            computerScore += 0;
-        }
-        else {
-            playerScore += 0;
-            computerScore += 0;
-        }
-    }
+            let computerSelection = computerPlay();
 
-    if (playerScore > computerScore) {
-        alert("You win!")
-    }
-    else if (computerScore > playerScore){
-        alert("You lose!")
-    }
-    else {
-        alert("Draw")
-    }
+            playRound(button.value, computerSelection);
+
+            result = button.value;
+
+            if (result == "rock" && computerSelection == "paper") {
+                computerScore++;          
+            } 
+
+            if (result == "rock" && computerSelection == "scissors") {
+                playerScore++;         
+            } 
+
+            if (result == "rock" && computerSelection == "rock") {
+                playerScore += 0;
+                computerScore += 0;
+            } 
+            
+            if (result == "paper" && computerSelection == "rock") {
+                playerScore++;
+            } 
+
+            if (result == "paper" && computerSelection == "scissors") {
+                computerScore++;
+            } 
+
+            if (result == "paper" && computerSelection == "paper") {
+                playerScore += 0;
+                computerScore += 0;
+            }
+
+            if (result == "scissors" && computerSelection == "paper") {
+                playerScore++;           
+            }
+
+            if (result == "scissors" && computerSelection == "rock") {
+                computerScore++;           
+            } 
+
+            if (result == "scissors" && computerSelection == "scissors") {
+                playerScore += 0;
+                computerScore += 0;
+            }
+            
+            player.innerHTML = playerScore
+            computer.innerHTML = computerScore;
+
+            let body = document.querySelector("body");
+            let p = document.createElement("p");
+        
+            if (playerScore == 5) {
+                p.textContent = "You Win!";           
+            } else if (computerScore == 5) {
+                p.textContent = "You Lose!";
+            }
+
+            const buttons = document.querySelectorAll('button');
+            if (playerScore == 5 || computerScore == 5) {
+                buttons.forEach((button) => {
+                    button.disabled = true;
+                });
+            }
+
+            body.appendChild(p);           
+        });
+    });
 }
 
 game();
